e422b7e0d0f4f3597321b597ff0413a11ccdff90
Andrey Shorokhov <arshor@rambler.ru>
2023-03-30 17:44:58 +0500

    First commit

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/home")
+    public String home(@RequestParam(required = false) String color, @RequestParam(required = false) String name, Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
+        return "home.html";
+    }
+
+    @RequestMapping("/home/{color}")
+    public String home_path(@PathVariable String color, Model page) {
+        page.addAttribute("username", "Katy");
+        page.addAttribute("color", color);
+        return "home.html";
+    }
+}
---
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String viewProducts(Model model) {
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+
+    @PostMapping("/products")
+    public String addProducts(@RequestParam String name, @RequestParam double price, Model model) {
+        Product p = new Product();
+        p.setName(name);
+        p.setPrice(price);
+        productService.addProduct(p);
+
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+}
