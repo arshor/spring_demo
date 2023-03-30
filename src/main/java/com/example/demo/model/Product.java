debd447ff6143dd04d6ee65a50c9eb587fded2a8
Andrey Shorokhov <arshor@rambler.ru>
2023-03-30 17:52:59 +0500

    Added files

---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+public class Product {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    private double price;
+
+}
