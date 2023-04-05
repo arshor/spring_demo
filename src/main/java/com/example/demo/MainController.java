5eb63cbc7d870fa5875e761de38e427a5ab469de
Andrey Shorokhov <arshor@rambler.ru>
2023-04-05 18:34:00 +0500

    Added LoginCountService

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import com.example.demo.service.LoggedUserManagementService;
+import com.example.demo.service.LoginCountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MainController {
 
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
 
-    public MainController(LoggedUserManagementService loggedUserManagementService) {
+    public MainController(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     @GetMapping("/main")
@@ -24,10 +27,13 @@ public class MainController {
         }
 
         String username = loggedUserManagementService.getUsername();
+        int count = loginCountService.getCount();
         if (username == null) {
             return "redirect:/";
         }
 
+        model.addAttribute("username", username);
+        model.addAttribute("loginCount", count);
         return "main.html";
     }
 
--- src/main/java/com/example/demo/model/LoginProcessor.java
+++ src/main/java/com/example/demo/model/LoginProcessor.java
@@ -1,6 +1,7 @@
 package com.example.demo.model;
 
 import com.example.demo.service.LoggedUserManagementService;
+import com.example.demo.service.LoginCountService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
@@ -9,23 +10,28 @@ import org.springframework.web.context.annotation.RequestScope;
 public class LoginProcessor {
 
     private final LoggedUserManagementService loggedUserManagementService;
+    private final LoginCountService loginCountService;
 
     private String username;
     private String password;
 
-    public LoginProcessor(LoggedUserManagementService loggedUserManagementService) {
+    public LoginProcessor(LoggedUserManagementService loggedUserManagementService, LoginCountService loginCountService) {
         this.loggedUserManagementService = loggedUserManagementService;
+        this.loginCountService = loginCountService;
     }
 
     public boolean login() {
         String username = this.getUsername();
         String password = this.getPassword();
+        loginCountService.increment();
 
+        boolean loginResult = false;
         if ("natalie".equals(username) && "password".equals(password)) {
-            return true;
-        } else {
-            return false;
+            loginResult = true;
+            loggedUserManagementService.setUsername(username);
         }
+        return loginResult;
+
     }
 
     public String getUsername() {
