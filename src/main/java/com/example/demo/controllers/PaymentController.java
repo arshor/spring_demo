6bccf00c8f7ca289f0b20a6ed3f8398c6e4d1427
Andrey Shorokhov <arshor@rambler.ru>
2023-04-13 10:32:17 +0500

    Added Payment class

--- src/main/java/com/example/demo/controllers/PaymentController.java
+++ src/main/java/com/example/demo/controllers/PaymentController.java
@@ -2,14 +2,17 @@ package com.example.demo.controllers;
 
 import com.example.demo.model.ErrorDetails;
 import com.example.demo.model.NotEnoughMoneyException;
+import com.example.demo.model.Payment;
 import com.example.demo.model.PaymentDetails;
 import com.example.demo.service.PaymentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
 import java.util.logging.Logger;
 
 @RestController
@@ -23,15 +26,28 @@ public class PaymentController {
 
     private static Logger logger = Logger.getLogger(PaymentController.class.getName());
 
+    //    @PostMapping("/payment")
+//    public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails) {
+//
+//        //PaymentDetails paymentDetails = paymentService.processPayment();
+//        logger.info("Received payment " + paymentDetails.getAmount());
+//
+//        return ResponseEntity
+//                .status(HttpStatus.ACCEPTED)
+//                .body(paymentDetails);
+//
+//    }
     @PostMapping("/payment")
-    public ResponseEntity<PaymentDetails> makePayment(@RequestBody PaymentDetails paymentDetails) {
+    public ResponseEntity<Payment> creatPayment(@RequestHeader String requestId, @RequestBody Payment payment) {
 
-        //PaymentDetails paymentDetails = paymentService.processPayment();
-        logger.info("Received payment " + paymentDetails.getAmount());
+        logger.info("Received request with ID " + requestId + " :Payment Amount: " + payment.getAmount());
+
+        payment.setId(UUID.randomUUID().toString());
 
         return ResponseEntity
-                .status(HttpStatus.ACCEPTED)
-                .body(paymentDetails);
+                .status(HttpStatus.OK)
+                .header("requestId", requestId)
+                .body(payment);
 
     }
 }
